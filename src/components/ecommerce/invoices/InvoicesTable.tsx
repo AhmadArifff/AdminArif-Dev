79fd221384a06876c2a580b22b2f354fefb6b127
vergenscande <mlqvftp123@gmail.com>
2026-08-02 13:31:07 +0700

    fix: change value to defaultValue in InvoicesTable

--- src/components/ecommerce/invoices/InvoicesTable.tsx
+++ src/components/ecommerce/invoices/InvoicesTable.tsx
@@ -33,7 +33,7 @@ export const InvoicesTable = () => {
        </path>
       </svg>
      </span>
-     <input className="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 h-11 w-full rounded-lg border border-gray-300 bg-transparent py-2.5 pr-4 pl-11 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden xl:w-[300px] dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30" placeholder="Search..." type="text" value=""/>
+     <input className="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 h-11 w-full rounded-lg border border-gray-300 bg-transparent py-2.5 pr-4 pl-11 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden xl:w-[300px] dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30" placeholder="Search..." type="text" defaultValue=""/>
     </div>
     <div className="relative">
      <button className="shadow-theme-xs flex h-11 w-full items-center justify-center gap-2 rounded-lg border border-gray-300 bg-white px-4 py-2.5 text-sm font-medium text-gray-700 sm:w-auto sm:min-w-[100px] dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400" type="button">
