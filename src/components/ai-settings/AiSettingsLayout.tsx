a63f28642aeeffbd796241bba99172f5dd7bbcd9
vergenscande <mlqvftp123@gmail.com>
2026-08-02 12:15:08 +0700

    fix: remove gap between main sidebar and AI settings sidebar

--- src/components/ai-settings/AiSettingsLayout.tsx
+++ src/components/ai-settings/AiSettingsLayout.tsx
@@ -7,7 +7,7 @@ import { MobileHeader } from "./MobileHeader";
 
 export const AISettingsLayout = () => {
   return (
-    <div className="relative overflow-hidden bg-white p-5 xl:flex xl:h-[calc(100vh-76px)] xl:p-0 dark:border-gray-800 dark:bg-gray-900">
+    <div className="relative overflow-hidden bg-white p-5 xl:flex xl:h-[calc(100vh-76px)] xl:p-0 dark:border-gray-800 dark:bg-gray-900 -mx-4 xl:-mx-6 -mt-6">
       <MobileHeader />
       <SettingsSidebar />
       <AccountSettings />
