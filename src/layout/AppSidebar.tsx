e80fd8890e749d5462391d6638c3968de1428fe4
AhmadArifff <aa2851214@gmail.com>
2026-08-04 17:28:24 +0700

    feat: add email inbox and details menu and pages based on design template

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -196,6 +196,18 @@ const supportItems: NavItem[] = [
       { name: "Ticket Reply", path: "/ticket-reply", pro: false },
     ],
   },
+  {
+    icon: (
+      <svg className="fill-current" width="20" height="20" viewBox="0 0 24 24" fill="none">
+        <path d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6zm-2 0l-8 5-8-5h16zm0 12H4V8l8 5 8-5v10z" />
+      </svg>
+    ),
+    name: "Email",
+    subItems: [
+      { name: "Inbox", path: "/inbox", pro: false },
+      { name: "Details", path: "/inbox-details", pro: false },
+    ],
+  },
 ];
 
 const AppSidebar: React.FC = () => {
