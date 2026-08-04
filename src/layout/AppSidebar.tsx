245aaa3b591b74baebb9d26aeeda73be99cde611
AhmadArifff <aa2851214@gmail.com>
2026-08-04 15:51:46 +0700

    feat: add faq menu and page based on design template

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -129,6 +129,7 @@ const navItems: NavItem[] = [
     subItems: [
       { name: "File Manager", path: "/file-manager", pro: true },
       { name: "Pricing Tables", path: "/pricing-tables", pro: true },
+      { name: "Faq", path: "/faq", pro: true },
       { name: "Blank Page", path: "/blank", pro: false },
       { name: "404 Error", path: "/error-404", pro: false },
     ],
